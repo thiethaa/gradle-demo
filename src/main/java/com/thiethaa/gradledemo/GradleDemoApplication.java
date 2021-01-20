59052662e7d06e77e83af16057207200ee03b5ea
atlroc99 <tyaniceluv89@gmail.com>
2021-01-19 21:05:29 -0500

    initial commit

---
+++ src/main/java/com/thiethaa/gradledemo/GradleDemoApplication.java
@@ -0,0 +1,20 @@
+package com.thiethaa.gradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GradleDemoApplication {
+
+    @GetMapping("/")
+    public String greeting(){
+        return "Hello Gradle.......!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(GradleDemoApplication.class, args);
+    }
+
+}
